bdc9370faea6ace795999c282022517cb2420eea
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-12-02 23:18:06 -0500

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -20,4 +20,7 @@ def start_server():
     # Configurar el servidor
     host = '127.0.0.1'
     port = 12345
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen(5)
 
