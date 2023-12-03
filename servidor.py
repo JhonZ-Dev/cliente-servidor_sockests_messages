ede53c623279787d8d1a32c9b662d4b27ba726e2
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-12-02 23:17:05 -0500

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -2,3 +2,7 @@ import socket
 import threading
 def handle_client(client_socket):
     while True:
+        # Recibir datos del cliente
+        data = client_socket.recv(1024)
+        if not data:
+            break
