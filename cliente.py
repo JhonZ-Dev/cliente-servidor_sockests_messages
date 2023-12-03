b8fc59dd43513525de89dfb94b35d0ce9da5af58
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-12-02 23:19:39 -0500

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -4,3 +4,5 @@ def start_client():
     # Configurar el cliente
     host = '127.0.0.1'
     port = 12345
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
