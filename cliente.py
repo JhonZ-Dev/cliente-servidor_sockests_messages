5c54a24668bec061ef35282ba40c219920c998fa
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-12-02 23:19:49 -0500

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -6,3 +6,8 @@ def start_client():
     port = 12345
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((host, port))
+    while True:
+        # Enviar un mensaje al servidor
+        message = input("Ingrese un mensaje para el servidor (o 'exit' para salir): ")
+        if message.lower() == 'exit':
+            break
